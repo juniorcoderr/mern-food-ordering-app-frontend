cac934977b3716384fd954699167ae95b1c3a4b2
Vinayak Yadav <netgoluyadav337@gmail.com>
2024-11-12 13:42:04 +0530

    modified homepage and searchbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
         <span className="text-xl">Food is just a click away!</span>
         <SearchBar
           placeHolder="Search by City or Town"
-          onSubmit={handleSearchSubmit} searchQuery={""}        />
+          onSubmit={handleSearchSubmit}  />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
